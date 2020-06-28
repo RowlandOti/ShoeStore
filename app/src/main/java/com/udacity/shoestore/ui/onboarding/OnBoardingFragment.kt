8dcda45b06a43d8f7e5217f44bf42760674bfdde
Otieno Rowland <r.otieno@kokonetworks.com>
2020-06-28 13:37:00 +0300

    refactor: move strings to xml

--- app/src/main/java/com/udacity/shoestore/ui/onboarding/OnBoardingFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/onboarding/OnBoardingFragment.kt
@@ -1,5 +1,6 @@
 package com.udacity.shoestore.ui.onboarding
 
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import androidx.navigation.fragment.findNavController
@@ -7,35 +8,48 @@ import com.ramotion.paperonboarding.PaperOnboardingFragment
 import com.ramotion.paperonboarding.PaperOnboardingPage
 import com.ramotion.paperonboarding.listeners.PaperOnboardingOnRightOutListener
 import com.udacity.shoestore.R
+import com.udacity.shoestore.R.string
 
 class OnBoardingFragment : PaperOnboardingFragment(), PaperOnboardingOnRightOutListener {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        elements = getOnBoardingScreens()
+        elements = getOnBoardingScreens(context)
+        setOnRightOutListener(this)
     }
 
     companion object {
-        private fun getOnBoardingScreens(): ArrayList<PaperOnboardingPage>? {
-            // prepare data
-            val scr1 = PaperOnboardingPage(
-                "Gym", "All hotels and hostels are sorted by hospitality rating",
-                Color.parseColor("#678FB4"), R.drawable.ic_gymnast_shoes, R.drawable.ic_gymnast_key
-            )
-            val scr2 = PaperOnboardingPage(
-                "Tennis", "We carefully verify all banks before add them into the app",
-                Color.parseColor("#65B0B4"), R.drawable.ic_tennis_shoe, R.drawable.ic_tennis_key
-            )
-            val scr3 = PaperOnboardingPage(
-                "Football", "All local stores are categorized for your convenience",
-                Color.parseColor("#9B90BC"), R.drawable.ic_football_shoe, R.drawable.ic_football_key
-            )
-            val scr4 = PaperOnboardingPage(
-                "Basketball", "Jump to skies as show as the great dunk of the century",
-                Color.parseColor("#9B90BC"), R.drawable.ic_basketball_shoe, R.drawable.ic_basketball_key
-            )
-            return arrayListOf(scr1, scr2, scr3, scr4)
+        private fun getOnBoardingScreens(ctx: Context?): ArrayList<PaperOnboardingPage>? {
+
+            val screens = arrayListOf<PaperOnboardingPage>()
+
+            ctx?.let {
+                // prepare data
+                val scr1 = PaperOnboardingPage(
+                    "Gym", ctx.getString(string.msg_gym),
+                    Color.parseColor("#678FB4"), R.drawable.ic_gymnast_shoes, R.drawable.ic_gymnast_key
+                )
+                val scr2 = PaperOnboardingPage(
+                    "Tennis", ctx.getString(string.msg_tennis),
+                    Color.parseColor("#65B0B4"), R.drawable.ic_tennis_shoe, R.drawable.ic_tennis_key
+                )
+                val scr3 = PaperOnboardingPage(
+                    "Football", ctx.getString(string.msg_football),
+                    Color.parseColor("#9B90BC"), R.drawable.ic_football_shoe, R.drawable.ic_football_key
+                )
+                val scr4 = PaperOnboardingPage(
+                    "Basketball", ctx.getString(string.msg_basketball),
+                    Color.parseColor("#9B90BC"), R.drawable.ic_basketball_shoe, R.drawable.ic_basketball_key
+                )
+
+                screens.add(scr1)
+                screens.add(scr2)
+                screens.add(scr3)
+                screens.add(scr4)
+            }
+
+            return screens
         }
     }
 

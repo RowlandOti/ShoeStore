c268fc3507d83c0622ca15970315737671384eac
Otieno Rowland <r.otieno@kokonetworks.com>
2020-06-28 13:19:58 +0300

    feat: added onboarding screen

--- app/src/main/java/com/udacity/shoestore/ui/onboarding/OnBoardingFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/onboarding/OnBoardingFragment.kt
@@ -1,30 +1,46 @@
 package com.udacity.shoestore.ui.onboarding
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.ramotion.paperonboarding.PaperOnboardingFragment
+import com.ramotion.paperonboarding.PaperOnboardingPage
+import com.ramotion.paperonboarding.listeners.PaperOnboardingOnRightOutListener
 import com.udacity.shoestore.R
 
-class OnBoardingFragment : Fragment() {
+class OnBoardingFragment : PaperOnboardingFragment(), PaperOnboardingOnRightOutListener {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onboarding, container, false)
+        elements = getOnBoardingScreens()
     }
 
     companion object {
+        private fun getOnBoardingScreens(): ArrayList<PaperOnboardingPage>? {
+            // prepare data
+            val scr1 = PaperOnboardingPage(
+                "Gym", "All hotels and hostels are sorted by hospitality rating",
+                Color.parseColor("#678FB4"), R.drawable.ic_gymnast_shoes, R.drawable.ic_gymnast_key
+            )
+            val scr2 = PaperOnboardingPage(
+                "Tennis", "We carefully verify all banks before add them into the app",
+                Color.parseColor("#65B0B4"), R.drawable.ic_tennis_shoe, R.drawable.ic_tennis_key
+            )
+            val scr3 = PaperOnboardingPage(
+                "Football", "All local stores are categorized for your convenience",
+                Color.parseColor("#9B90BC"), R.drawable.ic_football_shoe, R.drawable.ic_football_key
+            )
+            val scr4 = PaperOnboardingPage(
+                "Basketball", "Jump to skies as show as the great dunk of the century",
+                Color.parseColor("#9B90BC"), R.drawable.ic_basketball_shoe, R.drawable.ic_basketball_key
+            )
+            return arrayListOf(scr1, scr2, scr3, scr4)
+        }
+    }
 
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) = OnBoardingFragment()
+    override fun onRightOut() {
+        this.findNavController()
+            .navigate(OnBoardingFragmentDirections.toInstruction())
     }
 }

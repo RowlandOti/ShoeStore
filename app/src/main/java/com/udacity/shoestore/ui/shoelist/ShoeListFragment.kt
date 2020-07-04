9daec76ac4f6de71d04999a48a7e657b6844c48c
Otieno Rowland <r.otieno@kokonetworks.com>
2020-07-04 10:15:53 +0300

    feat: link to shoe detail screen

--- app/src/main/java/com/udacity/shoestore/ui/shoelist/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/shoelist/ShoeListFragment.kt
@@ -1,30 +1,43 @@
 package com.udacity.shoestore.ui.shoelist
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.udacity.shoestore.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
+import com.udacity.shoestore.databinding.FragmentShoelistBinding
 
 class ShoeListFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ShoeListFragment()
-    }
-
     private lateinit var viewModel: ShoeListViewModel
 
+    private lateinit var binding: FragmentShoelistBinding
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_shoelist, container, false)
+        binding = FragmentShoelistBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(ShoeListViewModel::class.java)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProviders.of(requireActivity()).get(ShoeListViewModel::class.java)
+
+        viewModel.getShoeList().observe(viewLifecycleOwner,
+            Observer {
+
+            })
+
+        binding.fab.setOnClickListener {
+            this.findNavController()
+                .navigate(ShoeListFragmentDirections.toShoeDetail())
+        }
     }
 }
--- app/src/main/java/com/udacity/shoestore/ui/shoelist/ShoeListViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/ui/shoelist/ShoeListViewModel.kt
@@ -1,7 +1,23 @@
 package com.udacity.shoestore.ui.shoelist
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.data.model.Shoe
 
 class ShoeListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val shoeList = MutableLiveData<MutableList<Shoe>>(mutableListOf())
+
+    fun getShoeList(): LiveData<MutableList<Shoe>> {
+        return shoeList
+    }
+
+    fun setShoeList(list: MutableList<Shoe>) {
+        shoeList.postValue(list)
+    }
+
+    fun addShoeToList(shoe: Shoe) {
+        shoeList.value?.add(shoe)
+    }
 }
